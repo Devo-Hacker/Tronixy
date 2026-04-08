6a6e01b6be7b7356b011a39330d000bd66f4f2de
Devo-hacker <niladreebiharinayak@gmail.com>
2026-04-08 14:30:07 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import colors from 'colors';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+//config the .env file
+dotenv.config();
+
+//rest object
+const app = express();
+
+//middleware
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
+
+//route  
+app.get("/", (req, res) => {  
+return res.status(200).send("<h1>Welcome To Node server</h1>");  
+});  
+  
+//port  
+const PORT = process.env.PORT || 8080;  
+  
+//listen  
+app.listen(PORT, () => {  
+console.log(`server running on PORT ${process.env.PORT}`.bgMagenta.black);  
+});  
