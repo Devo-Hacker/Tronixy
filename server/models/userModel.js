4bda223e5fcc5dafc3802ded2d88af562ebbc8ef
Devo-hacker <niladreebiharinayak@gmail.com>
2026-04-09 14:49:39 +0530

    user schema updated basic things

---
+++ server/models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'name is compulsory']
+    },
+    email:{
+        type:String,
+        required:[true, 'email is compulory'],
+        unique:[true, 'email already taken']
+    },
+    password:{
+        type:String,
+        required:[true, 'password is compulsory'],
+        minLength:[6, 'there should be atleast 6 characters']
+    },
+    address:{
+        type:String,
+        required:[true, 'address is compulsory']
+    },
+    city:{
+        type:String,
+        required:[true, 'enter your city name']
+    },
+    country:{
+        type:String,
+        required:[true, 'enter your country name']
+    },
+    phone:{
+        type:String,
+        required:[true, 'enter your phone number']
+    },
+    profilePic:{
+        type:String,
+    },
+},{timestamps:true});
+
+export const user = mongoose.model("Users", userSchema);
