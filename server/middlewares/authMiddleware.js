b78fb80ab7a7108894e73bcd9dabc64fb5fdd02d
Devo-hacker <niladreebiharinayak@gmail.com>
2026-04-10 22:04:19 +0530

    login logout private user profile updating successfully

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+import JWT from "jsonwebtoken";
+import userMdoel from "../models/userModel.js";
+
+// USER AUTH
+export const isAuth = async (req, res, next) => {
+  const { token } = req.cookies; //destructuring token
+  //valdiation
+  if (!token) {
+    return res.status(401).send({
+      success: false,
+      message: "UnAuthorized User",
+    });
+  }
+  const decodeData = JWT.verify(token, process.env.JWT_SECRET);
+  req.user = await userMdoel.findById(decodeData._id);
+  next(); //for decryping the data
+};
