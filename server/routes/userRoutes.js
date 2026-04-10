768e9a8f4163c55e2606e040a4104314fc8555b8
Devo-hacker <niladreebiharinayak@gmail.com>
2026-04-10 22:04:19 +0530

    login logout private user profile updating successfully

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { registerController, loginController } from '../controllers/userController.js';
+import { registerController, loginController, getUserProfileController, logoutController } from '../controllers/userController.js';
+import { isAuth } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -8,5 +9,11 @@ router.post('/register', registerController);
 
 //login
 router.post('/login', loginController);
+
+//profile
+router.get('/profile',isAuth, getUserProfileController);
+
+//logout
+router.get("/logout", isAuth, logoutController);
 
 export default router;
